f5d3c2de90bc101cd60f501ef0bf56adf3ef98aa
Exaltgod Mokonogho <mokonoghoexalt@gmail.com>
2025-03-06 17:36:00 +0100

    Finished code

--- day_14_Higer_low.py
+++ day_14_Higer_low.py
@@ -1,31 +1,19 @@
-from pi_art import higer_logo, vs
+from pi_art import higher_logo, vs, clear, higher_data
 import random
 
-higher_data = [
-    {
-        'name': 'Instagram',
-        'follower_count': 346,
-        'description': 'Social media platform',
-        'country': 'United States'
-    },
-    {
-        'name': 'Cristiano Ronaldo',
-        'follower_count': 215,
-        'description': 'Footballer',
-        'country': 'Portugal'
-    }]
-
-# print (higer_logo)
+print (higher_logo)
 
 first_data = random.choice(higher_data)
 
-second_data = random.choice(higher_data)
+def getting_B():
+    second_data = random.choice(higher_data)
+    while True:
+        if second_data == first_data:
+            second_data = random.choice(higher_data)
+        else:
+            break
+    return second_data
 
-while True:
-    if second_data == first_data:
-        second_data = random.choice(higher_data)
-    else:
-        break
 
 
 def compare(data):
@@ -34,59 +22,57 @@ def compare(data):
             follower = data[keys]
     return follower
 
-print(f"{first_data} \n {compare(first_data)}")
+def personality(data):
+    for keys in data:
+        if keys == "name":
+            name = data[keys]
+        elif keys == "description":
+            description = data[keys]
+        elif keys == "country":
+            country = data[keys]
+    detail_1 = f" {name}, a {description}, from {country}"
+    return detail_1
 
-print(f"{second_data} \n {compare(second_data)}")
+game = False
 
-# def personality(data):
-#     for keys in data:
-#         if keys == "name":
-#             name = data[keys]
-#         elif keys == "description":
-#             description = data[keys]
-#         elif keys == "country":
-#             country = data[keys]
-#     detail_1 = f" {name}, a {description}, from {country}"
-#     return detail_1
+print(f"Compare A {personality(first_data)}" )
 
+score = 0
 
-# game = False
+while not game:
+    print(vs)
+    second_data = getting_B()
 
-# print(f"Compare A {personality(first_data)}" )
+    print(f"Compare B: {personality(second_data)}")
 
+    follower1 = compare(first_data)
+    follower2 = compare(second_data)
 
-# while not game:
-#     print(vs)
+    choice = input("Who has more followers? Type 'A' or 'B': ")
 
-#     print(f"Compare B: {personality(second_data)}")
+    comparison = {
+        "A" : follower1,
+        "B": follower2
+    }
 
-#     choice = input("Who has more followers? Type 'A' or 'B': ")
-
-#     score = 0
-
-#     comparison = {
-#         "A" : compare(first_data),
-#         "B": compare(second_data)
-#     }
-
-#     score = comparison[choice]
-#     print(score)
-
-#     print(comparison["A"])
-#     print(comparison["B"])
-
-#     # if choice == "A":
-#     #     if comparison["A"]> comparison ["B"]:
-#     #         score += 1
-#     #         print(f"You're right, current score is {score}")
-#     #         print(f"Compare A {personality(first_data)}" )
-#     #     else:
-#     #         print(f"Sorry that's wrong, final score is {score}")
-#     #         game = True
-#     # elif choice == "B":
-#     #     if comparison[choice]> comparison ["A"]:
-#     #         score += 1
-#     #         print(f"Compare A {personality(second_data)}" )
-#     #     else:
-#     #         print(f"Sorry that's wrong, final score is {score}")
-#     #         game= True
+    if choice == "A":
+        if comparison["A"]> comparison ["B"]:
+            score += 1
+            clear()
+            print(higher_logo)
+            print(f"You're right, current score is {score}")
+            print(f"Compare A {personality(first_data)}" )
+        else:
+            print(f"Sorry that's wrong, final score is {score}")
+            game = True
+    elif choice == "B":
+        if comparison[choice]> comparison ["A"]:
+            first_data = second_data
+            score += 1
+            clear()
+            print(higher_logo)
+            print(f"You're right, current score is {score}")
+            print(f"Compare A {personality(second_data)}" )
+        else:
+            print(f"Sorry that's wrong, final score is {score}")
+            game= True
