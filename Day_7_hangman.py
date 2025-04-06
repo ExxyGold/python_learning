3ce65002e6adc0686752f172fcc17958cf50e2ef
Exaltgod Mokonogho <mokonoghoexalt@gmail.com>
2025-04-06 16:58:54 +0100

    updated hangman

--- Day_7_hangman.py
+++ Day_7_hangman.py
@@ -19,13 +19,23 @@ gameover = False
 while not gameover:
 
     guess= input("Choose a letter\n").lower()
+    
+
     for n in range(word_len):
         letter = chosen_word[n]
         if guess == letter:
             display[n] = letter
+            
+    letter_display = ""
+    for n in display:
+        letter_display += n
+        letter_display += " "
+
     if guess not in chosen_word:
-        lives-=1
-    print(f"{display} you have {lives}")
+        lives-=1 
+        print(f"{letter_display}\n you got it wrong, you have {lives} lives left")
+    else:
+        print(f"{letter_display}\n you got it Right, you have {lives} lives left")
     if lives == 0:
         gameover = True
         print("You Lose")
